ad93ef862037ad3619276b0f447c7f63ee6fd3ca
Jakada Harris <158389649+palefaceswiss@users.noreply.github.com>
2024-05-08 17:23:58 -0400

    Update and rename GraphicsQualityCalculator.java to GraphicsRecommendationToolPart1.java

--- GraphicsRecommendationToolPart1.java
+++ GraphicsRecommendationToolPart1.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class GraphicsQualityCalculator {
+public class GraphicsRecommendationToolPart1 {
    public static void main(String[] args){
    
    Scanner scanner = new Scanner(System.in);
